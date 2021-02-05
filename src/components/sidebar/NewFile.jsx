21a32e2559d5d1d0c29c57af4684e9637adb102b
ZenFarhat <s3777800@student.rmit.edu.au>
2021-02-05 16:25:57 +1100

    starting file uploading w/ firebase/GB

--- src/components/sidebar/NewFile.jsx
+++ src/components/sidebar/NewFile.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
 import "../../styles/NewFile.css";
+import firebase from "firebase";
+import { storage, db } from "../../firebase";
+import { makeStyles, MakeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+
+function getModalStyle() {
+  return {
+    top: `50%`,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: `absolute`,
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function NewFile() {
+  const classes = useStyles();
+
+  const [modalStyle] = useState(getModalStyle);
+
+  const [open, setOpen] = useState(false);
+  const [file, setfile] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
   return (
     <div className='newFile'>
       <div className='newFile__container'>
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSEYDfGtEtfl-bGKWFLPiXdUFsaXhQK-o",
+    authDomain: "online-drive-project.firebaseapp.com",
+    projectId: "online-drive-project",
+    storageBucket: "online-drive-project.appspot.com",
+    messagingSenderId: "960733958201",
+    appId: "1:960733958201:web:55b832f443b45927867de8",
+    measurementId: "G-5N9H75HMMV"
+  };
+
+  const firebaseApp = firebase.InitalizeApp(firebaseConfig)
+
+  const auth = firebase.auth()
+  const provider = new firebase.auth.GoogleAuthProvider()
+  const storage = firebase.sotrage()
+  const db  = firebaseApp.firestore()
+
+  export {auth, provider,db,storage}
